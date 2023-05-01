411edd1146fa4d43d5afb1ea72711f3a3cbfc9a5
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-01 18:48:57 -0300

    styles(switch): flex center

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import './App.css';
+import { ReactComponent as ListIcon } from './assets/listicon.svg';
 
 export default function App() {
   const [isOn, setIsOn] = useState(false);
   const toggleSwitch = () => setIsOn(!isOn);
 
   return (
+    <>
     <div className='switch' data-isOn={isOn} onClick={toggleSwitch}>
-      <motion.div className='handle' layout transition={spring}>
-        <motion.div className='bg' layout transition={spring} />
-      </motion.div>
+      {isOn ? (
+        <motion.div className='handle' layout transition={spring}>
+          <motion.div className='bg' layout transition={spring} />
+        </motion.div>
+      ) : (
+        <ListIcon />
+      )}
     </div>
+    </>
   )
 }
 
