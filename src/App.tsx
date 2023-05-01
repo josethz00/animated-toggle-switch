b749233a35d0e78699bb3eab1339ddcf46a2630d
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-01 18:37:07 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import './App.css';
+
+export default function App() {
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => setIsOn(!isOn);
+
+  return (
+    <div className='switch' data-isOn={isOn} onClick={toggleSwitch}>
+      <motion.div className='handle' layout transition={spring}>
+        <motion.div className='bg' layout transition={spring} />
+      </motion.div>
+    </div>
+  )
+}
+
+const spring = {
+  type: 'spring',
+  stiffness: 190,
+  damping: 30
+}
