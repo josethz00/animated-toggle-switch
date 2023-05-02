5199537be98a1265b83250e570711c43d31c9571
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-01 23:31:51 -0300

    lint: create editorconfig

--- src/components/CustomSelect/index.tsx
+++ src/components/CustomSelect/index.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 
 interface CustomSelectProps {
-    selectedOption?: string;
-    setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
-    children: React.ReactNode;
+  selectedOption?: string;
+  setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
+  children: React.ReactNode;
 }
 
 export default function CustomSelect({ selectedOption, setSelectedOption, children }: CustomSelectProps): JSX.Element {
-    return (
-        <select
-            value={selectedOption}
-            onChange={(e) => setSelectedOption(e.target.value)}
-            defaultValue='blue'
-            className='custom-select'
-        >
-            {children}
-        </select>
-    )
+  return (
+    <select
+      value={selectedOption}
+      onChange={(e) => setSelectedOption(e.target.value)}
+      defaultValue='blue'
+      className='custom-select'
+    >
+        {children}
+    </select>
+  )
 }
--- src/components/FancyToggleSwitch/index.tsx
+++ src/components/FancyToggleSwitch/index.tsx
@@ -3,86 +3,86 @@ import { ReactComponent as ListIcon } from '../../assets/listicon.svg';
 import { ReactComponent as GridIcon } from '../../assets/gridicon.svg';
 
 interface FancyToggleSwitchProps {
-    isOn: boolean;
-    setIsOn: (isOn: boolean) => void;
-    disabled?: boolean;
-    color?: string;
-    theme?: 'dark' | 'light';
+  isOn: boolean;
+  setIsOn: (isOn: boolean) => void;
+  disabled?: boolean;
+  color?: string;
+  theme?: 'dark' | 'light';
 }
 
 export default function FancyToggleSwitch({ isOn, setIsOn, disabled, color = 'blue', theme = 'dark'}: FancyToggleSwitchProps): JSX.Element {
-    const toggleSwitch = () => setIsOn(!isOn);
+  const toggleSwitch = () => setIsOn(!isOn);
 
-    const getHandleElementClassnames = () => {
-        const classNames = [' '];
-        if (color === 'red') {
-            classNames.push('handle-red');
-        }
-        if (isOn) {
-            if (color === 'red') {
-                classNames.push('semicircle-handle-red');
-            }
-            classNames.push('semicircle-handle');
-        }
-        return classNames.join(' ');
+  const getHandleElementClassnames = () => {
+    const classNames = [' '];
+    if (color === 'red') {
+      classNames.push('handle-red');
     }
-
-    const getBgElementClassnames = () => {
-        const classNames = [' '];
-        if (color === 'red') {
-            classNames.push('bg-red');
-        }
-        if (isOn) {
-            classNames.push('semicircle-bg');
-        }
-        return classNames.join(' ');
+    if (isOn) {
+      if (color === 'red') {
+        classNames.push('semicircle-handle-red');
+      }
+      classNames.push('semicircle-handle');
     }
+    return classNames.join(' ');
+  }
 
-    return (
-        <div
-            className={`switch ${theme === 'light' ? 'light-switch' : ''}`}
-            onClick={toggleSwitch}
-            data-isOn={isOn} style={{
-                pointerEvents: disabled ? 'none' : 'all',
-            }}
-        >
-            <motion.div
-                className={'handle' + getHandleElementClassnames()}
-                layout
-                transition={spring}
-                data-isOn={isOn}
-            >
-                <motion.div 
-                    className={'bg' + getBgElementClassnames()}
-                    layout
-                    transition={spring}
-                    data-isOn={isOn}
-                />
-            </motion.div>
-            {isOn ? (
-                <GridIcon
-                    width={67}
-                    height={67}
-                    className='icon'
-                    data-isOn={isOn}
-                    color={theme === 'light' ? '#585858' : '#FDFBF7'}
-                />
-                ) : (
-                <ListIcon
-                    width={67}
-                    height={67}
-                    className='icon'
-                    data-isOn={isOn}
-                    color={theme === 'light' ? '#585858' : '#FDFBF7'}
-                />
-            )}
-        </div>
-    )
+  const getBgElementClassnames = () => {
+    const classNames = [' '];
+    if (color === 'red') {
+      classNames.push('bg-red');
+    }
+    if (isOn) {
+      classNames.push('semicircle-bg');
+    }
+    return classNames.join(' ');
+  }
+
+  return (
+    <div
+      className={`switch ${theme === 'light' ? 'light-switch' : ''}`}
+      onClick={toggleSwitch}
+      data-isOn={isOn}
+      style={{
+        pointerEvents: disabled ? 'none' : 'all',
+      }}
+    >
+      <motion.div
+        className={'handle' + getHandleElementClassnames()}
+        layout
+        transition={spring}
+        data-isOn={isOn}
+      >
+        <motion.div
+          className={'bg' + getBgElementClassnames()}
+          layout
+          transition={spring}
+          data-isOn={isOn}
+        />
+      </motion.div>
+      {isOn ? (
+        <GridIcon
+          width={67}
+          height={67}
+          className='icon'
+          data-isOn={isOn}
+          color={theme === 'light' ? '#585858' : '#FDFBF7'}
+        />
+      ) : (
+        <ListIcon
+          width={67}
+          height={67}
+          className='icon'
+          data-isOn={isOn}
+          color={theme === 'light' ? '#585858' : '#FDFBF7'}
+        />
+      )}
+    </div>
+  )
 }
 
 const spring = {
-    type: 'spring',
-    stiffness: 190,
-    damping: 30,
+  type: 'spring',
+  stiffness: 190,
+  damping: 30,
 };
-          
